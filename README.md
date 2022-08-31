6516ce60be6b4dcd915621d813a7077037d4d706
michaelboegner <cmikeb@gmail.com>
2022-08-30 18:54:38 -0600

    Search party assembled and marching towards center

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# salesmanArc
+# Principle Solution for the Traveling Salesman Problem
+
+It is my understanding of the traveling salesman problem that the goal is to take a 
+given number of points and derive the shortest distance to visit all the points and 
+to only visit each point once.  Heuristically, I believe this can be solved using 
+what I am calling the shrink wrapping and search party method. 
+
+If you think of any object's surface as a 2d polygon, when viewed from the objects side, 
+if you were to shrink wrap that object with some plastic, the plastic would suck down on 
+to the surface of that object trying effectively to get to the center of the suction point. 
+Once shrunk wrapped, the plastic's location would then represent the shortest distance around 
+the circumference of that object. Thought of in 2d, you can extrapolate that to a circle and a 
+set of points, wherein you shrink wrap the points with the circle, and the resulting circumference 
+of that shape is the solution to the traveling salesman problem. 
+
+The search party portion of the solution comes in because I do not know of any way to collision detect the 
+circumference of the circle with the points in a software based setting. So instead, the following algorithm 
+takes the place of the real world shrink wrap application by resulting in the same shape, using effectively 
+the same principle as the shrink wrap portion, albeit with a less fluid or calculus based approach.
+
+## Search Party Algorithm for the Traveling Salesman Problem (screw acronyms)
+1. Sort the coordinates to find max and min correspondingly for each set of x and y. 
+2. Find the halfway points between the max and mins for each set of x and y, resulting in a set of coordinates, 
+   which gives you your collective center for your traveling salesman points. 
+3. Assemble a search party of points, the number of which should match the number of traveling salesman points, 
+   and assemble them in a circle, equidistant from each other, with the center point of that circle being our 
+   derived collective center from the previous step, and each search party point holding a line in each 
+   hand, each of which is connected to their adjacent neighbors who are also correspondingly also holding
+   a line in each hand, forming a circle. 
+4. Assign each search point a search distance equal to a radius the length of which is halfway to their 
+   adjacent neighbor.
+5. All search points move incrementally, stepwise towards the collective center, searching for a traveling salesman point
+   within each of their respective radii.
+6. If a search point discovers a traveling salesman point entering its search radius, then that search point 
+   should reassign its coordinates to the coordinates of the found point and hold its position and maintain the 
+   line connection to its neighbors.
+7. All search points should proceed towards the collective center until all have found a traveling salesman
+   point to hold to. The resulting shape, formed by their connecting lines, is the solution to the 
+   traveling salesman problem.
---
+++ main-finished.js
@@ -0,0 +1,248 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+class Circle {
+
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.lineWidth = 4;
+    }
+
+    drawPoint() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+        ctx.fill();
+    }
+
+    drawCenterPoint() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'rgba(15, 10, 222, 1)';
+        ctx.fill();
+    }
+}
+
+class SearchPartyPoint extends Circle {
+    
+    constructor(x, y, radius, pointRadiusSq){
+        super(x, y, radius);
+        this.pointRadiusSq = pointRadiusSq;
+    }
+
+    drawSearchPartyPoint() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'rgba(0, 128, 0, 1)';
+        ctx.fill();
+    }
+
+    drawSearchPartyPointRadius(searchPartyPointRadius) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, searchPartyPointRadius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    linkWithNeihbors() {
+
+    }
+
+    moveToCenter(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    searchRadius() {
+
+    }
+
+    foundAndHold() {
+
+    }
+
+
+}
+
+const salesmanPoints = 365;
+
+const points = []; 
+const searchPartyPoints = [];
+const centerX = width/2;
+const centerY = height/2;
+const circle = new Circle(centerX, centerY, 350);
+
+
+//Generate Traveling Salesman Points randomly
+while (points.length < salesmanPoints) {
+
+    let pointAngle = Math.random() * 2 * Math.PI;
+    let pointRadiusSq = Math.random() * (circle.radius-100) * circle.radius;
+    let pointX = Math.sqrt(pointRadiusSq) * Math.cos(pointAngle);
+    let pointY = Math.sqrt(pointRadiusSq) * Math.sin(pointAngle);
+    let locationX = pointX + centerX;
+    let locationY = pointY + centerY;
+
+    const point = new Circle(
+        locationX,
+        locationY,
+        1
+    )
+
+    points.push(point);  
+}
+
+
+
+let maxPointX = 0;
+let minPointX = width;
+let maxPointY = 0;
+let minPointY = height;
+let centerPointX = 0;
+let centerPointY = 0;
+let centerPoint;
+
+for (let i = 0; i < points.length; i++){
+    for (let h = 1; h < points.length; h++) {
+        
+        //max checker X
+        if (points[i].x > points[h].x && points[i].x > maxPointX) {
+            maxPointX = points[i].x;
+        } 
+    
+        //min checker X
+        if (points[i].x < points[h].x && points[i].x < minPointX) {
+            minPointX = points[i].x;
+        } 
+    
+        //max checker Y
+        if (points[i].y > points[h].y && points[i].y > maxPointY) {
+            maxPointY = points[i].y;
+        } 
+    
+        //min checker Y
+        if (points[i].y < points[h].y && points[i].y < minPointY) {
+            minPointY = points[i].y;
+        } 
+
+        centerPointX = ((maxPointX - minPointX) / 2) + minPointX;
+        centerPointY = ((maxPointY - minPointY) / 2) + minPointY;
+        
+        if (i === points.length - 1 && h === points.length - 1) {
+            centerPoint = new Circle(
+                centerPointX,
+                centerPointY,
+                10
+            )   
+        }
+
+    }
+}
+
+//Generate Search Party Points and assemble in equidistant circle around collective center
+let pointAngleCounter = 1;
+
+while (searchPartyPoints.length < points.length) {    
+    let pointAngle = 1/points.length * 2 * Math.PI;
+    let pointRadiusSq = circle.radius * circle.radius + 50000;
+    let pointX, pointY, pointAngleIncrement, locationX, locationY
+    
+    if (pointAngleCounter < points.length) {
+        pointAngleIncrement = pointAngle * pointAngleCounter
+        pointX = Math.sqrt(pointRadiusSq) * Math.cos(pointAngleIncrement);
+        pointY = Math.sqrt(pointRadiusSq) * Math.sin(pointAngleIncrement);
+        locationX = pointX + centerPointX;
+        locationY = pointY + centerPointY;
+    } 
+
+    const searchPartyPoint = new SearchPartyPoint (
+        locationX,
+        locationY,
+        3,
+        pointRadiusSq
+    )
+
+    searchPartyPoints.push(searchPartyPoint);
+    pointAngleCounter++
+}
+
+
+
+//Loop the animation using requestAnimationFrame()
+function loop() {
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+    ctx.fillRect(0, 0,  width, height);
+
+    if (searchPartyPoints[0].pointRadiusSq > 0) {
+        for (i = 0; i < searchPartyPoints.length; i++) {
+            let pointAngle = 1/searchPartyPoints.length * 2 * Math.PI;
+            let pointX, pointY, pointAngleIncrement, locationX, locationY
+            
+            pointAngleIncrement = pointAngle * i;
+            pointX = Math.sqrt(searchPartyPoints[i].pointRadiusSq) * Math.cos(pointAngleIncrement);
+            pointY = Math.sqrt(searchPartyPoints[i].pointRadiusSq) * Math.sin(pointAngleIncrement);
+            locationX = pointX + centerPointX;
+            locationY = pointY + centerPointY;
+     
+            searchPartyPoints[i].pointRadiusSq = searchPartyPoints[i].pointRadiusSq - 1000;
+            searchPartyPoints[i].drawSearchPartyPoint();
+            searchPartyPoints[i].moveToCenter(locationX, locationY);
+            centerPoint.drawCenterPoint(); 
+        }
+
+        for (i = 0; i < points.length - 1; i++) {
+            points[i].drawPoint(); 
+        }
+    } else {
+        for (i = 0; i < searchPartyPoints.length; i++) {
+            searchPartyPoints[i].drawSearchPartyPoint();
+            centerPoint.drawCenterPoint();
+        }
+
+        for (i = 0; i < points.length; i++) {
+            points[i].drawPoint();
+        }
+    }
+
+    requestAnimationFrame(loop)
+}
+
+loop();
+
+//connect Search Party Points to each other with lines
+// for (i = 0; i <= searchPartyPoints.length; i++) {
+//     if(i === 0) {
+//         console.log('first')
+//         ctx.beginPath()
+//         ctx.moveTo(searchPartyPoints[i].x, searchPartyPoints[i].y);
+//     } else if(i > 0 && i < searchPartyPoints.length) {
+//         console.log('second')
+//         ctx.lineTo(searchPartyPoints[i].x, searchPartyPoints[i].y);
+//     } else {
+//         console.log('third')
+//         ctx.closePath();
+//     }
+//     ctx.stroke()
+// }
+
+// //Draw Search Party Point's radius
+// const searchPartyPointRadius = (Math.sqrt(((searchPartyPoints[1].x-searchPartyPoints[0].x) * ((searchPartyPoints[1].x-searchPartyPoints[0].x))) + ((searchPartyPoints[1].y-searchPartyPoints[0].y) * ((searchPartyPoints[1].y-searchPartyPoints[0].y))))) / 2;
+// console.log(searchPartyPointRadius, 'searchPartyPointRadius')
+
+// for (i = 0; i < searchPartyPoints.length; i++) {
+//     searchPartyPoints[i].drawSearchPartyPointRadius(searchPartyPointRadius);
+// }
